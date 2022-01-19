d3b821e9ff9e76eeefe743415f10baa9632d5308
Sezer Esim <sezeresim@gmail.com>
2022-01-19 22:06:32 +0300

    Deleted defaults

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,11 +3,9 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  // !STARTERCONF Change the siteUrl
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: 'www.example.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
   },
-};
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,25 +7,12 @@ import UnderlineLink from '@/components/links/UnderlineLink'
 import UnstyledLink from '@/components/links/UnstyledLink'
 import Seo from '@/components/Seo'
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
 import Vercel from '~/svg/Vercel.svg'
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
 
 export default function HomePage() {
   return (
     <Layout>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
-
+      <Seo templateTitle='Home' />
       <main>
         <section className='bg-white'>
           <div className='flex flex-col items-center justify-center layout min-h-screen text-center'>
