e021c1cc0a6cb9cd8d917b793b93461e91ce0e97
Sezer Esim <sezeresim@gmail.com>
2023-09-04 22:28:25 +0300

    OG

--- src/components/OGImage/OgImage.tsx
+++ src/components/OGImage/OgImage.tsx
@@ -9,28 +9,72 @@ const OgImage = (props: Props) => {
   return (
     <div
       style={{
-        fontSize: 128,
-        background: 'white',
+        display: 'flex',
         width: '100%',
         height: '100%',
-        display: 'flex',
-        alignItems: 'flex-end',
-        justifyContent: 'flex-start',
+        background: 'linear-gradient(to right, #e8cbc0, #636fa4)',
+        padding: '32px',
       }}
     >
-      {props.title}
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
+          display: 'flex',
           width: '100%',
           height: '100%',
-          display: 'flex',
-          alignItems: 'flex-end',
-          justifyContent: 'flex-start',
+          alignItems: 'center',
+          padding: '64px',
         }}
       >
-        {props.description}
+        <div
+          style={{
+            display: 'flex',
+            height: '100%',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div style={{ display: 'flex', marginTop: '80px' }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                marginLeft: '48px',
+              }}
+            >
+              <span
+                style={{
+                  fontSize: '56px',
+                  color: '#222',
+                  paddingTop: '32px',
+                  fontFamily: "'Inter', sans-serif",
+                  fontWeight: 700,
+                }}
+              >
+                {props.title}
+              </span>
+              <span
+                style={{
+                  fontSize: '28px',
+                  color: '#222',
+                  fontFamily: "'Inter', sans-serif",
+                  fontWeight: 500,
+                }}
+              >
+                Software Engineer
+              </span>
+            </div>
+          </div>
+          <span
+            style={{
+              fontSize: '18px',
+              color: '#222',
+              fontFamily: "'Inter', sans-serif",
+              fontWeight: 500,
+            }}
+          >
+            {props.description}
+          </span>
+        </div>
       </div>
     </div>
   );
